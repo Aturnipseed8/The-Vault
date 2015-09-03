c92a163b721e24add4fd86bed7030e3fb5acf1d4
Anna <anna.turnipseed@gmail.com>
2015-09-02 19:43:58 -1000

    Public and private variable placement tripped me up until now

--- vault.js
+++ vault.js
@@ -1,4 +1,30 @@
+//Sets 'secret' values defined by a 'key' word.
+//Create a module that exposes 2 functions.
+
+
 'use strict';
 module.exports = function() {
-  
+ 	var _key = '';
+ 	var _value; 
+ 
+//  1) Vault should return null if no key is used:
+	function getValue(key) {
+		if(_key === key) {
+			return _value;
+		} else {
+			return null;
+		}
+	}
+ // }
+ 	function setValue(key, value) {
+ 		_key = key;
+ 		_value = value;
+
+ }
+return {
+	setValue : setValue,
+	getValue : getValue
+};
+
+
 };
